d26f45eadd3bdb3b8cc347828d27d95b0584cb18
ADITYA RAJ <141955081+1amAdityaa@users.noreply.github.com>
2026-09-15 09:45:30 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+
+
+        // we have adjacency matrix here
+        // here we have to focus on direction
+
+
+        int row = grid.length;
+        int col = grid[0].length;
+
+        // we have too perform dfs oon each and every nooode 
+      
+        int maxarea=0;
+        for(int i = 0;i<row ; i++)
+        {
+            for(int j = 0;j<col;j++)
+            {
+                if(grid[i][j]==1)
+                { 
+                maxarea=Math.max(maxarea,  dfs(i,j,grid));
+                }
+            }
+        }
+        return maxarea; //mistake
+        
+    }
+
+    private static int dfs(int row,int col,int[][] grid)//char mistake
+    {
+        int newrow=grid.length;
+        int newcol=grid[0].length;
+
+        //now we have focus on movement direction
+
+        int [][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        
+        int area=1;
+        if(row<0 || col<0 || newrow<=row || newcol<=col || grid[row][col]==0) return 0; //boolean mistake
+
+        grid[row][col]=0; //visited node
+        for(int [] dir: directions)
+        {
+            area+= dfs(row+dir[0],col+dir[1],grid); //full argument passing bro
+        }
+        return area;
+
+    }
+
+}
+
+        
+        
+
+
